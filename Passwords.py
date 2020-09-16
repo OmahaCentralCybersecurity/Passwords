1ec26322a7b85fcced2347f3f70ee9f649904991
Collin Holmquist <collinholmquist@gmail.com>
2020-09-15 23:46:37 -0500

    Create Passwords.py

---
+++ Passwords.py
@@ -0,0 +1,14 @@
+import string
+
+# generate should take a length and the return a password of random lowercase letters (or uppercase / digits / puncuation)
+
+def generate(length):
+  pass
+
+# import the password cracker file
+from PasswordCracker import *
+
+# the function password_crack now becomes available.  
+# pass the generated password 
+
+
