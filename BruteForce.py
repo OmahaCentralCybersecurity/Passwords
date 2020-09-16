5e39b81447650fd9cd66ddc24671c37b508e19b3
Collin Holmquist <collinholmquist@gmail.com>
2020-09-16 12:42:54 -0500

    Create BruteForce.py

---
+++ BruteForce.py
@@ -0,0 +1,16 @@
+from PasswordGenerate import generate
+
+# prompt user for password length and then generate a random sequence of the given length 
+passLen = int(input("How many characters for your password?: "))
+password = generate(passLen)
+
+def brute_force(len(password)):
+  pass
+
+
+
+#program
+brute_force(passLen)
+
+
+
