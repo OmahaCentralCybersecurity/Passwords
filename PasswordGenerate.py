e5dec7fe23e3f8b4108cf7da18322255d9b80f29
Collin Holmquist <collinholmquist@gmail.com>
2020-09-16 07:58:31 -0500

    Update and rename Passwords.py to PasswordGenerate.py

---
+++ PasswordGenerate.py
@@ -0,0 +1,11 @@
+import string, random
+
+# generate should take a length and the return a password of random letters and digits. Puncuation is optional. 
+
+def generate(length):
+  pass
+
+
+generate()
+
+
