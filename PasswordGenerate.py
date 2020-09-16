00a4a6cdabfd1f4cfe5f928385cf2a8257c9932e
Collin Holmquist <collinholmquist@gmail.com>
2020-09-16 12:36:06 -0500

    Update PasswordGenerate.py

--- PasswordGenerate.py
+++ PasswordGenerate.py
@@ -3,8 +3,13 @@ import string, random
 # generate should take a length and the return a password of random letters and digits. Puncuation is optional. 
 
 def generate(length):
-  pass
-
+  characters = string.ascii_letters + string.digits
+  password = ''
+  
+  for i in range(length):
+    rand = random.randint(0, len(characters))
+    password = password + characters[rand]
+    return password
 
 generate()
 
