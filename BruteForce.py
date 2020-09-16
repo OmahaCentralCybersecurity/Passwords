dab3d60d966b19200fab4b9e3c4da028cff9c06b
Collin Holmquist <collinholmquist@gmail.com>
2020-09-16 20:10:53 +0000

    brute force attack

--- BruteForce.py
+++ BruteForce.py
@@ -1,16 +1,36 @@
 from PasswordGenerate import generate
+import string, itertools
+import time
 
-# prompt user for password length and then generate a random sequence of the given length 
-passLen = int(input("How many characters for your password?: "))
-password = generate(passLen)
+def brute_force(passLen, password):
 
-def brute_force(len(password)):
-  pass
+    start_time = time.time()
 
+    guess = ""
+    correct = False
+
+    ### Change me to try various character combinations
+    characters = string.ascii_lowercase + string.ascii_uppercase
+
+    for letters in itertools.product(characters, repeat = passLen):
+        # letter is formatted as ('char', 'char') so use .join to create one string. 
+        letters = ''.join(letters)
+        #print(letters)
+        if letters == password:
+            guess = letters
+    
+    end_time = time.time()
+    total_time = end_time - start_time
+    return guess, total_time
 
 
 #program
-brute_force(passLen)
+# prompt user for password length and then generate a random sequence of the given length 
+passLen =  int(input("How many characters for your password?: "))
+generated_password = generate(passLen)
+print(generated_password)
+guessed_pwd, time_taken = brute_force(passLen, generated_password)
 
+print("The password",guessed_pwd, "took", time_taken, " seconds to guess. ")
 
 
--- PasswordGenerate.py
+++ PasswordGenerate.py
@@ -3,15 +3,14 @@ import string, random
 # generate should take a length and the return a password of random letters and digits. Puncuation is optional. 
 
 def generate(length):
-  characters = string.ascii_letters + string.digits
-  password = ''
+    characters = string.ascii_lowercase + string.ascii_uppercase # + string.digits
+    create_password = ''
   
-  for i in range(length):
-    rand = random.randint(0, len(characters))
-    password = password + characters[rand]
+    for i in range(length):
+        rand = random.randint(0, len(characters)- 1)
+        create_password = create_password + characters[rand]
     
-  return password
+    return create_password
 
-generate()
 
 
